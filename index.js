d15a6da4638368edc613e03226b748e8c0278f0a
wuxxy <33169104+wuxxy@users.noreply.github.com>
2025-04-03 13:54:47 -0400

    TW-7

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function start(){
     const {words, characterTree} = loadText();
     const wordAmount = words.length;
     for(let w = 0;w<characterTree.length;w++){
-        typewriter.innerHTML += `<span id="typewriter-word-${w}">`;
+        typewriter.innerHTML = typewriter.innerHTML + `<span id="typewriter-word-${w}">`;
         for(let c = 0;c<characterTree[w].length;c++){
             typewriter.innerHTML += `<span id="typewriter-char-${c}">${characterTree[w][c]}</span>`;
         }
