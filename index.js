27891fa58c7a79d3a4c9e44d4c6c50af0d23aa02
wuxxy <33169104+wuxxy@users.noreply.github.com>
2025-04-04 12:07:36 -0400

    Test

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ function handleChar(e){
   if(disallowedCodes.includes(key)) return;
   typer.innerHTML = "";
   if(key == "Backspace" || key == "Delete") {
-    typed[index-1]="";
+    typed.splice(index, 1);
     if(index > 0) index--;
   }else{
     typed += key;
